deb85a12042d55cc814df8c8d9dbe0248a37880e
colinsbarrett <colinsbarrett@hotmail.com>
2025-05-21 07:32:35 +0100

    Add flowchart pages

--- app/routes.js
+++ app/routes.js
@@ -39,3 +39,101 @@ router.post('/passport-details', function (req, res) {
     res.redirect('/rest-test')
   }
 })
+// Ash steps
+router.get('/smri-declaration', function (req, res) {
+  res.render('smri-declaration')
+})
+
+// Did not add AND make sure this file ends with: module.exports = router
+
+// Move to next page
+router.post('/smri-declaration', function (req, res) {
+  res.redirect('/smri-fit1') 
+})
+// Next 1
+router.get('/smri-fit1', function (req, res) {
+  res.render('smri-fit1')
+})
+
+router.post('/smri-fit1', function (req, res) {
+  const answer = req.body.smri1
+
+  if (answer === 'yes') {
+    res.redirect('/smri-fit2-yes')
+  } else {
+    res.redirect('/smri-fit2-no')
+  }
+})
+// Next 2
+router.get('/smri-fit2-yes', function (req, res) {
+  res.render('smri-fit2-yes')
+})
+
+router.post('/smri-fit2-yes', function (req, res) {
+  const answer = req.body.smri2
+
+  if (answer === 'yes') {
+    res.redirect('/smri-fit3-yes')
+  } else {
+    res.redirect('/smri-fit3-no')
+  }
+})
+// Next 3 yes post was smri-fit2-yes
+router.get('/smri-fit3-yes', function (req, res) {
+  res.render('smri-fit3-yes')
+})
+
+router.post('/smri-fit3-yes', function (req, res) {
+  const answer = req.body.smri3
+
+  if (answer === 'yes') {
+    res.redirect('/smri-fit4-yes')
+  } else {
+    res.redirect('/smri-fit4-no')
+  }
+})
+
+// Next 3 no
+
+router.get('/smri-fit3-no', function (req, res) {
+  res.render('smri-fit3-no')
+})
+
+router.post('/smri-fit3-no', function (req, res) {
+  res.redirect('/smri-declaration') 
+})
+
+// Next 4 yes
+router.get('/smri-fit4-yes', function (req, res) {
+  res.render('smri-fit4-yes')
+})
+
+router.post('/smri-fit4-yes', function (req, res) {
+  const answer = req.body.smri4
+
+  if (answer === 'yes') {
+    res.redirect('/smri-fit5-yes')
+  } else {
+    res.redirect('/smri-fit5-no')
+  }
+})
+
+// Next 5 yes
+
+router.get('/smri-fit5-yes', function (req, res) {
+  res.render('smri-fit5-yes')
+})
+
+router.post('/smri-fit5-yes', function (req, res) {
+  res.redirect('/smri-end') 
+})
+
+// Next 5 yes
+
+router.get('/smri-end', function (req, res) {
+  res.render('smri-end')
+})
+
+router.post('/smri-end', function (req, res) {
+  res.redirect('/smri-declaration') 
+})
